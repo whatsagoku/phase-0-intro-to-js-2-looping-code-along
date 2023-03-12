de931e22e8784ebcceeebd5d49efe85c8b0d7d7d
Jeffery Hutchinson <jefferyshutchinson@gmail.com>
2023-03-12 10:17:51 -0400

    Loopty Loop!

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function writeCards(names) {
+    const thankYouMessages = [];
+    for (const name of names) {
+      thankYouMessages.push(`Thank you, ${name}, for the wonderful surprise gift!`);
+    }
+    return thankYouMessages;
+  }
+  
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
+  const names = ["Guadalupe", "Ollie", "Aki"];
+  const thankYouMessages = writeCards(names);
+  console.log(thankYouMessages);
+  
+  countDown(5);
